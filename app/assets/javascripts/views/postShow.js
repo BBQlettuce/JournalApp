0ab7ca890ea8c35f8a4037f825d96e297961752e
oscar + aiven <oscar+aiven@aa.io>
2015-09-09 17:33:28 -0400

    implement fancy title & body edit

--- app/assets/javascripts/views/postShow.js
+++ app/assets/javascripts/views/postShow.js
@@ -5,7 +5,11 @@ JournalApp.Views.PostShow = Backbone.View.extend({
 
   events: {
     "click .edit-post": "editPost",
-    "click .delete-post": "deletePost"
+    "click .delete-post": "deletePost",
+    "dblclick .title": "editTitle",
+    "dblclick .body": "editBody",
+    "blur .title": "updateTitle",
+    "blur .body": "updateBody"
   },
 
   template: JST['postShow'],
@@ -23,5 +27,37 @@ JournalApp.Views.PostShow = Backbone.View.extend({
   deletePost: function() {
     this.model.destroy();
     Backbone.history.navigate("", {trigger: true})
+  },
+
+  editTitle: function(e) {
+    e.preventDefault();
+    var $title = $(e.currentTarget);
+    var $input = $("<input>");
+    $input.attr("type", "text").attr("name", "post[title]").val($title.html());
+    $title.html($input);
+  },
+
+  editBody: function(e) {
+    e.preventDefault();
+    var $body = $(e.currentTarget);
+    var $textarea = $("<textarea>");
+    $textarea.attr("name", "post[body]").val($body.html());
+    $body.html($textarea);
+  },
+
+  updateTitle: function(e) {
+    debugger 
+    e.preventDefault();
+    var newTitle = $(e.target).val();
+
+    this.model.save({ title: newTitle });
+  },
+
+  updateBody: function(e) {
+    debugger
+    e.preventDefault();
+    var newBody = $(e.target).val();
+
+    this.model.save({ body: newBody });
   }
 });
